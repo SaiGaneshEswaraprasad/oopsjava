cef0042290c02d117c39b2d8484f017be346b4bf
SaiGaneshEswaraprasad <113225851+SaiGaneshEswaraprasad@users.noreply.github.com>
2024-01-12 09:21:20 +0000

    done

--- th1m3.java
+++ th1m3.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class 1mod3 {
+public class th1m3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
